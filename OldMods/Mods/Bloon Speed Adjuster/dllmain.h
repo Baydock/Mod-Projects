b425431747117e2f79ac28f0b5384c70d3e9365a
baydock <41644363+baydock@users.noreply.github.com>
2020-11-12 10:46:31 -0600

    removed old mods

--- OldMods/Mods/Bloon Speed Adjuster/dllmain.h
+++
@@ -1,9 +0,0 @@
-// Generated C++ file by Il2CppInspector - http://www.djkaty.com - https://github.com/djkaty
-// Edited by Baydock for the Bloon Speed Adjuster Mod for BTD6
-
-// Add mod to window
-LRESULT CALLBACK ModProc(HWND, UINT, WPARAM, LPARAM);
-void ModSetup(HWND, HINSTANCE);
-
-// IL2CPP initializer
-void init_il2cpp();
--- OldMods/Mods/Bloon Speed Adjuster/win32main.h
+++
@@ -1,5 +0,0 @@
-// Created by Baydock for the Bloon Speed Adjuster Mod for Btd6
-
-// Entry point declaration for custom injected code
-bool Run();
-bool Start();
