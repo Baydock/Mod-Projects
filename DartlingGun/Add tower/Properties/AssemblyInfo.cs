708d54ab83a63b0335b30d3393155014f61b6485
baydock <41644363+baydock@users.noreply.github.com>
2020-11-12 11:48:14 -0600

    Revert "hi"
    This reverts commit 4b9f702ffd17ef0f7576c78a98303a349b4c0230.

---
+++ DartlingGun/Add tower/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using DartlingGun;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(DartlingGunMod), "Dartling Gun", "1.1.0", "Baydock")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Dartling Gun")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dartling Gun")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b3ab9efe-1f2d-418e-a450-db5fc05148eb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
---
+++ Look At Player/Look At Player/Look At Player.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Unity.Bridge;
+using Assets.Scripts.Unity.UI_New.InGame;
+using Harmony;
+using MelonLoader;
+using UnityEngine;
+
+namespace LookAtPlayer {
+
+    public class LookAtPlayerMod : MelonMod { }
+
+    [HarmonyPatch(typeof(InGame), "Update")]
+    public class Update_Patch {
+        [HarmonyPostfix]
+        public static void Postfix() {
+            if (InGame.Bridge == null) return;
+            foreach (TowerToSimulation tts in InGame.Bridge.GetAllTowers()) {
+                Camera camera = InGame.instance.sceneCamera;
+                tts?.tower?.Node?.graphic?.transform.LookAt(camera.ScreenToWorldPoint(
+                    new Vector3(camera.pixelWidth / 2, camera.pixelHeight / 2, camera.nearClipPlane)));
+            }
+        }
+    }
+}
